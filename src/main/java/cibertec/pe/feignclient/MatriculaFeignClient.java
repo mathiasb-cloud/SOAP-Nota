1625c1d8639941c3b1fb8dfae00d8f22a2bac277
Mathias Benites <i202406644@cibertec.edu.pe>
2026-02-28 11:54:48 -0500

    hola

---
+++ src/main/java/cibertec/pe/feignclient/MatriculaFeignClient.java
@@ -0,0 +1,13 @@
+package cibertec.pe.feignclient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import cibertec.pe.entity.Matricula;
+
+@FeignClient(name = "SOAP-Matricula", url = "${feign.cuenta.url}")
+public interface MatriculaFeignClient {
+	
+	@GetMapping("/api/matriculas/{id}")
+	ResponseEntity<Matricula> obtenerMatricula(@PathVariable("id") int id);
+}
---
+++ src/main/java/cibertec/pe/service/INotaService.java
@@ -0,0 +1,22 @@
+package cibertec.pe.service;
+import java.util.List;
+import java.util.Optional;
+
+import cibertec.pe.dto.ResultadoDTO;
+import cibertec.pe.model.Nota;
+import jakarta.jws.WebService;
+
+@WebService
+public interface INotaService {
+	
+	public List<Nota> listarNotas();
+	public Nota crearNota(Nota nota);
+	public Optional<Nota> buscarNota(int codNota);
+	public String editarNota(int codNota, Nota nota);
+	public void eliminarNota(int codNota);
+	public List<Nota> listarNotasPorMatricula(int codMatricula);
+	public List<Nota> listarNotasPorAlumno(int codAlumno);
+	public List<Nota> listarNotasPorCurso(int codCurso);
+	
+	ResultadoDTO calcularResultado(int codMatricula);
+}
